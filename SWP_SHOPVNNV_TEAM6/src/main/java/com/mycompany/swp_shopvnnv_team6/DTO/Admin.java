e59efa045be0e986326398d91c3a8332581ced19
TuanVinh1304 <155642829+TuanVinh1304@users.noreply.github.com>
2024-01-26 10:42:44 +0700

    Create Admin.java

---
+++ SWP_SHOPVNNV_TEAM6/src/main/java/com/mycompany/swp_shopvnnv_team6/DTO/Admin.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.swp_shopvnnv_team6.DTO;
+
+/**
+ *
+ * @author duong
+ */
+public class Admin {
+    private String nameString;
+}
